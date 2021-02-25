5d05831511691866cf4a32a00510fa601030f669
Leah Miller <leahmiller1969@gmail.com>
2021-02-25 09:44:00 -0800

    using anonymous function to reduce lines of code

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-document.querySelector("button").addEventListener('click', handleClick);
+// document.querySelector("button").addEventListener('click', handleClick);
+
+// function handleClick() {
+//     alert('heyo');
+// }
+
 //handleClick is not passed as a function, but as an input, because we don't want the function to run when the browser first reads it
 // we want to pass the function to then be run when the correct button ('click') is clicked.
 
 
-function handleClick() {
+//this works the same as above, but with much less code!
+document.querySelector('button').addEventListener('click', function () {
     alert('heyo');
-}
+})
+
